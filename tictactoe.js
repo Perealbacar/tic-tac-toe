0142eaee7e0ce8ac0ea4cede1b195c7cd4a79032
Perealbacar <37050221+Perealbacar@users.noreply.github.com>
2024-10-23 17:54:01 +0200

    Added basic board functionality and playerInput

--- tictactoe.js
+++ tictactoe.js
@@ -1,8 +1,39 @@
-function Hello(){
+/*function Hello(){
     console.log("Hello World!");
     const titulo = document.querySelector("div");
     titulo.textContent = "Hello World!";
     
 }
+*/
 
-Hello();
+const Hello = function (name){
+    console.log("Hello " + name);
+    const titulo = document.querySelector("div");
+    titulo.textContent = "Hello " + name;
+}
+
+
+Hello("lala");
+
+const Gameboard = function(){
+    let board = [];
+
+    for(let i = 0 ; i < 8; i++){
+        board.push(" ");
+    }
+    console.log(board);
+
+    const playerInput = function(position){
+        board[position - 1] = "x";    
+    }
+    return {board, playerInput}
+}
+
+const lala = Gameboard();
+
+//player A = x
+//player B = o
+
+//round++
+
+//
